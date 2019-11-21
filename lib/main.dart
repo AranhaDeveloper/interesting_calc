52a77ff3bc26a160dd94a76dc6bbbf3df8658c58
AranhaDeveloper <aranha.br@gmail.com>
2019-11-21 08:17:05 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Simple Interested Calculator App',
+    home: SIForm(),
+  ));
+}
+
+class SIForm extends StatefulWidget {
+  @override
+  _SIFormState createState() => _SIFormState();
+}
+
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees', 'Dolars', 'Pounds'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Interesting Calculator'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+          Text('Teste')],
+        ),
+      ),
+    );
+  }
+}
